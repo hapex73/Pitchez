d4dec572fe2aeb9e00845c7735f477964155a36d
Hannu Hirvi <hannu.hirvi@softability.fi>
2013-04-29 15:15:51 +0300

    Created dummy test by MbUnit

---
+++ MbUnitTest/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿namespace MbUnitTest
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Fibonacci
+    {
+        /// <summary>
+        /// Calculates the specified x.
+        /// </summary>
+        /// <param name="x">The x.</param>
+        /// <returns></returns>
+        public static int Calculate(int x)
+        {
+            if (x <= 0)
+                return 0;
+
+            if (x == 1)
+                return 1;
+
+            return Calculate(x - 1) + Calculate(x - 2);
+        }
+    }
+}
---
+++ MbUnitTest/FibonacciTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MbUnitTest;
+using MbUnit.Framework;
+
+namespace MbUnitTest.Test
+{
+    public class FibonacciTest
+    {
+        [Test]
+        public void Fibonacci_of_number_greater_than_one()
+        {
+            int result = Fibonacci.Calculate(6);
+            Assert.AreEqual(8, result);
+        }
+    }
+}
